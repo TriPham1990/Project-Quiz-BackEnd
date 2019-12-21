f4c78abe852d5f377c9a82250dd135c59e2918c5
Tri Pham <pmtri@gmail.com>
2019-12-22 02:05:54 +0700

    cap nhat controller cho answer

--- src/main/java/tri/lo/controller/AnswerController.java
+++ src/main/java/tri/lo/controller/AnswerController.java
@@ -3,10 +3,12 @@ package tri.lo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import tri.lo.model.quiz.Answer;
+import tri.lo.model.quiz.Question;
 import tri.lo.service.quiz.AnswerService;
 
 import java.util.List;
@@ -33,6 +35,16 @@ public class AnswerController {
     private ResponseEntity<?> createAnswer(@RequestBody Answer answer) {
         answerService.save(answer);
         return new ResponseEntity<>(answer, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/findByQuestion/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<List<Answer>> getAllQuestionByCategoryId(@PathVariable Long id) {
+        List<Answer> answers = answerService.findAllByQuestionId(id);
+        if (answers.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(answers, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id")
